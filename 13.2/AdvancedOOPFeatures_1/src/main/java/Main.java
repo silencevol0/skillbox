c2ff2482805d034d181f085ef60e31cd68cf1490
silencevol0 <silence.alex.true@gmail.com>
2022-11-26 18:22:46 +0300

    13.2

--- 13.2/AdvancedOOPFeatures_1/src/main/java/Main.java
+++ 13.2/AdvancedOOPFeatures_1/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -6,10 +8,18 @@ public class Main {
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
-        System.out.println(staff);
+        sortBySalaryAndAlphabet(staff);
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        Collections.sort(staff, (o1, o2) -> {
+            int c;
+            c = o1.getSalary().compareTo(o2.getSalary());
+            if (c == 0) return o1.getName().compareTo(o2.getName());
+            else return o1.getSalary().compareTo(o2.getSalary());
+        });
     }
 }
